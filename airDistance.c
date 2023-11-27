83492d4a22f1ed9d4bc5de1d3f0365e867a4dbbe
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-28 01:26:18 +0500

    Added functionality to getAirDistance function like delta and airdistance and we well find distance value using spherical law

---
+++ airDistance.c
@@ -0,0 +1,8 @@
+#include<math.h>
+double getAirDistance(double originLatitude,double originLongitude,
+                      double destinationLatitude,double destinationLongitude){
+    double delta = destinationLongitude - originLongitude;
+    float const R = 6371;
+    double distance = acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) *cos(delta)) * R;
+    
+}
