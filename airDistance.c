6daf077da4718f727a8ee5217378d427dbfa2b68
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-28 01:32:54 +0500

    We add the utils maded header for to convert degree to redian

--- airDistance.c
+++ airDistance.c
@@ -1,8 +1,9 @@
 #include<math.h>
+#include<"utils.h">
 double getAirDistance(double originLatitude,double originLongitude,
                       double destinationLatitude,double destinationLongitude){
     double delta = destinationLongitude - originLongitude;
     float const R = 6371;
     double distance = acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) *cos(delta)) * R;
-    
+    return distance;
 }
