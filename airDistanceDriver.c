341392665f22fbd7445a610f5470d80ff227cd53
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-28 01:49:28 +0500

    Made utils for header and airDistanceDriver for to run the file in main

---
+++ airDistanceDriver.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+ double airDistance;
+ double originLatitude,originLongitude;
+ double destinationLatitude,destinationLongitude;
+ printf("Enter the Latitude and Longitude of Origin(latitude,longitude): ");
+ scanf("%lf,%lf",&originLatitude,&originLongitude);
+ printf("Enter the Latitude and Longitude of Destination(latitude,longitude): ");
+ scanf("%lf,%lf",&destinationLatitude,&destinationLongitude);
+ airDistance = getAirDistance(originLatitude,originLongitude,destinationLatitude,destinationLongitude);
+ printf("Air Distance  = %0.2lf\n",airDistance);
+
+}
