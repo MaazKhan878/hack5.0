937725f3261817ef0e9c5974565836fe1358e113
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-29 01:53:53 +0500

    add the third test to test the function

--- airDistanceTester.c
+++ airDistanceTester.c
@@ -6,18 +6,48 @@ int main(){
     int passed = 0;
     double latitude = 50;
     double returnedValue = round(degreesToRadians(latitude));
-    double result = 0.872665;
     if(returnedValue == 1){
      printf("PASSED\n");
      passed++;
     }
     else{
-     printf("Failed: degreeToRadians returned %lf, Expected 0.872665\n",returnedValue);
+     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
      failed++;
    
     }
-  printf("The number of test passed: %d\n",passed);
-  printf("The number of test failed: %d\n",failed);
-  printf("The percentage of test passed: %lf\n",100.0 * passed/(passed + failed));
+    if(returnedValue == 1){
+     printf("PASSED\n");
+     passed++;
+    }
+    else{
+     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
+     failed++;
+   
+    }
+    latitude = 150;
+    returnedValue = round(degreesToRadians(latitude));
+    if(returnedValue == 1){
+     printf("PASSED\n");
+     passed++;
+    }
+    else{
+     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
+     failed++;
+   
+    }
+    int longitude = 96;
+    returnedValue = round(degreesToRadians(longitude));
+    if(returnedValue == 1){
+     printf("PASSED\n");
+     passed++;
+    }
+    else{
+     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
+     failed++;
+   
+    }
+    printf("The number of test passed: %d\n",passed);
+    printf("The number of test failed: %d\n",failed);
+    printf("The percentage of test passed: %lf\n",100.0 * passed/(passed + failed));
   
 }
