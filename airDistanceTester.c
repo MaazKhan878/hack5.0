8d8b751eb3898e060ffb217340157f0870400a9a
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-28 12:43:43 +0500

    add math laibrary and round the returned value and edit to show passed and failed and show percentage

--- airDistanceTester.c
+++ airDistanceTester.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
+#include<math.h>
 #include"utils.h"
 int main(){
     int failed = 0;
     int passed = 0;
-    double latitude = 40;
-    if(degreesToRadians(latitude)!=5){
-    printf("Failed: degreeToRadians returned %lf, Expected 6\n",degreesToRadians(latitude));
-    failed++;
-  }
-  else{
-    printf("PASSED\n");
-    passed++;
-  }
+    double latitude = 50;
+    double returnedValue = round(degreesToRadians(latitude));
+    double result = 0.872665;
+    if(returnedValue == 1){
+     printf("PASSED\n");
+     passed++;
+    }
+    else{
+     printf("Failed: degreeToRadians returned %lf, Expected 0.872665\n",returnedValue);
+     failed++;
+   
+    }
+  printf("The number of test passed: %d\n",passed);
+  printf("The number of test failed: %d\n",failed);
+  printf("The percentage of test passed: %lf\n",100.0 * passed/(passed + failed));
+  
 }
