30afea112d521beac3c37c0964fe8c3420c37d5c
Maaz_Khan <maazkhan0348810@gmail.com>
2023-11-30 19:35:53 +0500

    reduce coding

--- timeDilation.c
+++ timeDilation.c
@@ -2,9 +2,6 @@
  #include<math.h>
  double lorentzTimeDilation(double t, double percentC){
     double T;
-    //c = 1 finglonger/zoidberg.
-    double c = 1.0;
-    double v = percentC * c;
-    T = t/sqrt(1.0 - pow(v, 2)/pow(c, 2));
+    T = t/sqrt(1.0 - pow(percentC, 2));
     return T;
  }
