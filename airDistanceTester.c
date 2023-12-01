ba895705e78289012125c670aab5a62720c9cd3e
Maaz_Khan <maazkhan0348810@gmail.com>
2023-12-02 04:00:43 +0500

    Add the isClose Function to airDistanceTester.c

--- airDistanceTester.c
+++ airDistanceTester.c
@@ -3,41 +3,48 @@
 #include<stdio.h>
 #include<math.h>
 #include"utils.h"
+#define DELTA 0.0001
+int isClose(double a , double b){
+    return (fabs(a - b) < DELTA);
+}
 int main(){
     int failed = 0;
     int passed = 0;
     // test cases for the function degree to radians 
     double latitude = 50.0; // take argument with 50 
-    double returnedValue = round(degreesToRadians(latitude)); // the argument latitude value copy to the function parameter
-    if(returnedValue == 1){
+    double expected = 0.872665;
+    double returnedValue = (degreesToRadians(latitude)); // the argument latitude value copy to the function parameter 
+    if(isClose(returnedValue,expected) ){
      printf("PASSED\n"); // test passed
      passed++;
     }
     else{
-     printf("Failed: degreeToRadians returned %lf, Expected 3\n",returnedValue);
+     printf("Failed: degreeToRadians returned %lf, Expected %lf \n",returnedValue, expected);
      failed++; // test failed
    
     }
     // same for other 2 test case just change value
     latitude = 150.0;
-    returnedValue = round(degreesToRadians(latitude));
-    if(returnedValue == 3){
+    expected = 2.617994;
+    returnedValue = (degreesToRadians(latitude));
+    if(isClose(returnedValue, expected)){
      printf("PASSED\n");
      passed++;
     }
     else{
-     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
+     printf("Failed: degreeToRadians returned %lf, Expected %lf \n",returnedValue, expected);
      failed++;
    
     }
     double longitude = 96.0;
+    expected = 2.0;
     returnedValue = round(degreesToRadians(longitude));
-    if(returnedValue == 2){
+    if(isClose(returnedValue, expected)){
      printf("PASSED\n");
      passed++;
     }
     else{
-     printf("Failed: degreeToRadians returned %lf, Expected 1\n",returnedValue);
+      printf("Failed: degreeToRadians returned %lf, Expected %lf \n",returnedValue, expected);
      failed++;
    
     }
